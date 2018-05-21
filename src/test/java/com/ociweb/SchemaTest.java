59c231f5e2386809c3b74f195d3a37c0a624e874
Tobias Schweiger <schweigert@objectcomputing.com>
2018-05-21 13:35:37 -0500

    expanded pipe writing tests to demonstrate low/hi level apis

--- src/test/java/com/ociweb/SchemaTest.java
+++ src/test/java/com/ociweb/SchemaTest.java
@@ -1,12 +1,12 @@
 package com.ociweb;
 
+import static com.ociweb.pronghorn.pipe.Pipe.takeRingByteLen;
+import static com.ociweb.pronghorn.pipe.Pipe.takeRingByteMetaData;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.ociweb.pronghorn.ExampleProducerStage;
-import com.ociweb.pronghorn.pipe.ChannelReader;
-import com.ociweb.pronghorn.pipe.ChannelWriter;
-import com.ociweb.pronghorn.pipe.DataOutputBlobWriter;
-import com.ociweb.pronghorn.pipe.Pipe;
+import com.ociweb.pronghorn.pipe.*;
 import com.ociweb.pronghorn.stage.PronghornStage;
 import com.ociweb.pronghorn.stage.route.ReplicatorStage;
 import com.ociweb.pronghorn.stage.scheduling.GraphManager;
@@ -18,6 +18,7 @@ import com.ociweb.SchemaOneSchema;
 import com.ociweb.pronghorn.pipe.util.build.FROMValidation;
 
 import javax.xml.validation.Schema;
+import java.io.IOException;
 
 public class SchemaTest {
 
@@ -29,47 +30,6 @@ public class SchemaTest {
     public void messageClientNetResponseSchemaFROMTest() {
     	
         assertTrue(FROMValidation.checkSchema("/SchemaOne.xml", SchemaOneSchema.class));
-
-    }
-
-    /**
-     * Creates a new GraphManager and demonstrates how to write to a pipe directly using low-level API.
-     * Then, verifies that content in output pipe from replicator is the same as content in input pipe.
-     */
-    @Test
-    void checkPrepopulatedPipeData() {
-
-        GraphManager gm = new GraphManager();
-
-        Pipe inputPipe = SchemaOneSchema.instance.newPipe(50, 500);
-        Pipe outputPipe = SchemaOneSchema.instance.newPipe(50, 500);
-
-        //Pipe outputPipe = PipeNoOp.newInstance()
-
-        inputPipe.initBuffers();
-
-        int size = Pipe.addMsgIdx(inputPipe, SchemaOneSchema.MSG_SOMEOTHERMESSAGE_2);
-
-        ChannelWriter cw = Pipe.openOutputStream(inputPipe);
-
-        cw.writeInt(1000);
-        cw.writeLong(500000000);
-
-        ((DataOutputBlobWriter) cw).closeLowLevelField();
-
-        Pipe.confirmLowLevelWrite(inputPipe, size);
-        Pipe.publishWrites(inputPipe);
-
-        //Do we still need a scheduler?
-
-
-        ReplicatorStage.newInstance(gm, inputPipe, outputPipe);
-
-        //NOW, read what's written onto the outputPipe!
-
-
-
-        gm.enableTelemetry(7779);
 
     }
 	
